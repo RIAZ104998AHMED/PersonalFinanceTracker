ec1cdda6dc1d0e94426e445235484ce4b1aa085e
Ahmed Riaz <riazahmed5231@gmail.com>
2026-09-20 22:33:54 +0700

    Configure Swagger UI for API testing

--- Program.cs
+++ Program.cs
@@ -1,22 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add controller support
 builder.Services.AddControllers();
-
-// Add OpenAPI support
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Enable OpenAPI in development
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
-// Connect controller routes
 app.MapControllers();
 
 app.Run();
