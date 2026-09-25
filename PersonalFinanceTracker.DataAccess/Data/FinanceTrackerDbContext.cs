e032064ce35e1e8246779dfac910e1a5a6bdc1a4
Ahmed Riaz <riazahmed5231@gmail.com>
2026-09-25 21:36:03 +0700

    Configure EF Core and PostgreSQL packages

---
+++ PersonalFinanceTracker.DataAccess/Data/FinanceTrackerDbContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PersonalFinanceTracker.DataAccess.Entities;
+
+namespace PersonalFinanceTracker.DataAccess.Data;
+
+public class FinanceTrackerDbContext : DbContext
+{
+    public FinanceTrackerDbContext(
+        DbContextOptions<FinanceTrackerDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Account> Accounts => Set<Account>();
+
+    public DbSet<Category> Categories => Set<Category>();
+
+    public DbSet<Operation> Operations => Set<Operation>();
+
+    public DbSet<Transfer> Transfers => Set<Transfer>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Account>()
+            .Property(a => a.StartAmount)
+            .HasPrecision(18, 2);
+
+        modelBuilder.Entity<Operation>()
+            .Property(o => o.Amount)
+            .HasPrecision(18, 2);
+
+        modelBuilder.Entity<Transfer>()
+            .Property(t => t.Amount)
+            .HasPrecision(18, 2);
+
+        modelBuilder.Entity<Account>()
+            .Property(a => a.Currency)
+            .HasMaxLength(3);
+    }
+}
